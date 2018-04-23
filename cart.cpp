afbb22321e05816cb824f49aef7791499244ab05
Linus <linusg@hotmail.se>
2018-04-23 22:21:04 +0200

    Initial stuff

---
+++ cart.cpp
@@ -0,0 +1,14 @@
+#include "include/cart.h"
+
+void model::Cart::UpdateCart(std::string ean, unsigned short amount) {
+  /* TODO: Everything */
+  cart_.insert({ean, amount}); 
+}
+
+model::cartmap::const_iterator model::Cart::ItBegin() {
+  return cart_.cbegin();
+}
+
+model::cartmap::const_iterator model::Cart::ItEnd() {
+  return cart_.cend();
+}
